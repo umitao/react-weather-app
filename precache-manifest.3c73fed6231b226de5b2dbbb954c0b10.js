50cfbf74e7d57b7ccc6203e808a5cfc65c61d0a1
umitao <umitao@gmail.com>
2020-06-17 19:17:49 +0300

    Updates

---
+++ precache-manifest.3c73fed6231b226de5b2dbbb954c0b10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40462e69b4eb1bf327fd7c8257477dc7",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "edd382866a0e3a6c9f22",
+    "url": "/react-weather-app/static/css/main.04bba97c.chunk.css"
+  },
+  {
+    "revision": "5d23a32050ca62a6e773",
+    "url": "/react-weather-app/static/js/2.23f298e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-app/static/js/2.23f298e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edd382866a0e3a6c9f22",
+    "url": "/react-weather-app/static/js/main.865513e2.chunk.js"
+  },
+  {
+    "revision": "a165d426a6e734b12112",
+    "url": "/react-weather-app/static/js/runtime-main.ec1803de.js"
+  }
+]);
